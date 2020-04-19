d2fb552d850019ce59e31ca5fe8690514e2c3727
james <jtwebco@gmail.com>
2020-04-19 17:24:30 -0400

    port

--- stellr.js
+++ stellr.js
@@ -4,7 +4,7 @@ const express = require("express")
 const cors = require("cors")
 const bodyParser = require("body-parser")
 const app = express()
-const port = 8080
+const port = 5000
 const Twit = require("twit")
 const T = new Twit({
   consumer_key: process.env.TW_CONSUMER,
